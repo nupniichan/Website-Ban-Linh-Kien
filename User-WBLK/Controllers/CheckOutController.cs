9915b3dbab05687d5b9ed842f439b7bc6df87b41
The Pink Cat <asdsaphat@gmail.com>
2025-01-24 09:49:49 +0700

    (tạm) hoàn thành giao diện checkout

--- User-WBLK/Controllers/CheckOutController.cs
+++ User-WBLK/Controllers/CheckOutController.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace Website_Ban_Linh_Kien.Controllers
 {
@@ -10,5 +9,33 @@ namespace Website_Ban_Linh_Kien.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult PlaceOrder(OrderInfo orderInfo)
+        {
+            // Xử lý đặt hàng ở đây
+            return RedirectToAction("OrderConfirmation");
+        }
+    }
+
+    public class CartItem
+    {
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public class OrderInfo
+    {
+        public string CustomerName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Province { get; set; }
+        public string District { get; set; }
+        public string Address { get; set; }
+        public string Note { get; set; }
+        public string PaymentMethod { get; set; }
+        public string DiscountCode { get; set; }
+        public bool IsHomeDelivery { get; set; }
     }
 }
