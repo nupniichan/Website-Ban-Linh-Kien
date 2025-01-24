63df68a9bdb5baf7daa682d2bce7c6bc819d1eb9
The Pink Cat <asdsaphat@gmail.com>
2025-01-25 00:17:13 +0700

    Thay đổi css -> tailwind + xóa toàn bộ data tạm vì không sử dụng tới

--- User-WBLK/Controllers/CheckOutController.cs
+++ User-WBLK/Controllers/CheckOutController.cs
@@ -9,33 +9,5 @@ namespace Website_Ban_Linh_Kien.Controllers
         {
             return View();
         }
-
-        [HttpPost]
-        public ActionResult PlaceOrder(OrderInfo orderInfo)
-        {
-            // Xử lý đặt hàng ở đây
-            return RedirectToAction("OrderConfirmation");
-        }
-    }
-
-    public class CartItem
-    {
-        public string ProductName { get; set; }
-        public int Quantity { get; set; }
-        public decimal Price { get; set; }
-    }
-
-    public class OrderInfo
-    {
-        public string CustomerName { get; set; }
-        public string Email { get; set; }
-        public string Phone { get; set; }
-        public string Province { get; set; }
-        public string District { get; set; }
-        public string Address { get; set; }
-        public string Note { get; set; }
-        public string PaymentMethod { get; set; }
-        public string DiscountCode { get; set; }
-        public bool IsHomeDelivery { get; set; }
     }
 }
