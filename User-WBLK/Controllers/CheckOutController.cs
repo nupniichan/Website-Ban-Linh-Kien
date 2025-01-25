66a09806cf4a31cdb7d724ff289d0c77f6da767e
nupniichan <bao96065@gmail.com>
2025-01-25 11:19:02 +0700

    feat(ui): improve checkout UI

--- User-WBLK/Controllers/CheckOutController.cs
+++ User-WBLK/Controllers/CheckOutController.cs
@@ -1,13 +1,52 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Website_Ban_Linh_Kien.Models;
 
 namespace Website_Ban_Linh_Kien.Controllers
 {
-    public class CheckOutController : Controller
+    public class CheckOutController : BaseController
     {
-        // GET: CheckOutController
+        private const string STORE_ADDRESS = "123 Nguyễn Văn A, Quận 1, TP.HCM";
+
         public ActionResult Index()
         {
-            return View();
+            SetBreadcrumb(
+                ("Checkout", "/checkout"),
+                ("Checkout", null)
+            );
+            
+            var model = new CheckoutViewModel
+            {
+                DeliveryMethod = DeliveryMethod.HomeDelivery, // Mặc định chọn giao hàng
+                StoreAddress = STORE_ADDRESS
+            };
+            
+            // Có thể thêm code để lấy thông tin người dùng đã đăng nhập
+            if (User.Identity.IsAuthenticated)
+            {
+                // Lấy thông tin từ user profile
+                model.ReceiverName = User.Identity.Name;
+                // model.ReceiverPhone = currentUser.Phone;
+            }
+            
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult SaveDeliveryInfo(CheckoutViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Index", model);
+            }
+
+            // Xử lý địa chỉ giao hàng
+            string fullAddress = model.DeliveryMethod == DeliveryMethod.StorePickup 
+                ? STORE_ADDRESS 
+                : $"{model.StreetAddress}, {model.Ward}, {model.District}, {model.City}";
+
+            // TODO: Lưu thông tin đơn hàng vào database
+            // Tạm thời chuyển hướng đến trang thanh toán
+            return RedirectToAction("Payment");
         }
     }
 }
